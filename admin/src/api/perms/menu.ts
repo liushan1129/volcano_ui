a141fc4f90511f32a96f9eaa75502ed432240d86
liushan09 <liushan09@maoyan.com>
2023-04-09 12:45:19 +0800

    volcano前端代码 init

---
+++ admin/src/api/perms/menu.ts
@@ -0,0 +1,29 @@
+import request from '@/utils/request'
+
+// 菜单列表
+export function menuLists(params?: Record<string, any>) {
+    return request.get({ url: '/system/menu/list', params })
+}
+export function optionalMenus(params?: Record<string, any>) {
+    return request.get({ url: '/system/menu/getOptionalMenus', params })
+}
+
+// 添加菜单
+export function menuAdd(params: Record<string, any>) {
+    return request.post({ url: '/system/menu/add', params })
+}
+
+// 编辑菜单
+export function menuEdit(params: Record<string, any>) {
+    return request.post({ url: '/system/menu/edit', params })
+}
+
+// 菜单删除
+export function menuDelete(params: Record<string, any>) {
+    return request.post({ url: '/system/menu/del', params })
+}
+
+// 菜单删除
+export function menuDetail(params: Record<string, any>) {
+    return request.get({ url: '/system/menu/detail', params })
+}
